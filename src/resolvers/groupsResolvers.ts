d02356f70f7f9ff6d28194f6dbbdf10f30a60587
arojasag <arojasag@unal.edu.co>
2025-06-26 22:28:24 -0500

    Fix getImage so that it returns null when encountering a 404. It should do this because profilePics aren't mandatory. Alternatively, we could always return a placeholder when groups have no profile pic, but that would require a modification in the backend

--- src/resolvers/groupsResolvers.ts
+++ src/resolvers/groupsResolvers.ts
@@ -3,14 +3,16 @@
  */
 import { UUID } from "node:crypto";
 import { Buffer } from "node:buffer";
-import { fetchMS, URL_TYPES, URLS } from "../fetchMicroservices.js";
+import { fetchMS, unwrappedFetchMS, URL_TYPES, URLS } from "../fetchMicroservices.js";
 import { Image } from "../types.js";
+import { GraphQLError } from "graphql";
+import { ErrorCodes } from "../errorHandling.js";
 
 interface Group {
     id: UUID;
     name: string;
     description: string;
-    profilePic: Image;
+    profilePic?: Image;
     isVerified: boolean;
     isOpen: boolean;
     createdAt: Date;
@@ -34,9 +36,26 @@ export interface CreateGroup {
     isOpen: boolean;
 }
 
-const getImage = async (url: string) => {
-    const response = await fetchMS({ url, responseType: URL_TYPES.JPEG });
 
+
+const getImage = async (url: string) => {
+    // It is necessary to use unwrappedFetchMS because fetchMS doesn't return a status code when throwing
+    // a GraphQLError, so we can't know if it failed because of a 404 and return null in that case
+    const response = await unwrappedFetchMS({ url, responseType: URL_TYPES.JPEG })
+
+    // TODO: ideally the backend should return some default image when a group doesn't
+    // have one, instead of having to return null here. This would also let us use fetchMS in getImage
+    if ( response.status == 404 ) {
+        return null
+    }
+    if ( response.status != 200 ) {
+        throw new GraphQLError(ErrorCodes.GENERIC_CLIENT_ERROR, {
+            extensions: {
+                code: ErrorCodes.GENERIC_CLIENT_ERROR,
+            },
+        });
+    }
+    
     const body = response.responseBody as Buffer;
     return body.toString("base64");
 };
